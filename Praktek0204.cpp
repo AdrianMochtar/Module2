decdd63802f43df641bc7c9ef1eca547de6a1339
AdrianMochtar <mochtaradrian.am@gmail.com>
2017-10-21 17:33:31 +0800

    menambahkan inisialisasi dan konversi

--- Praktek0204.cpp
+++ Praktek0204.cpp
@@ -5,5 +5,10 @@ int main(){
 	double luasrumah = 42;
 	double lantaiP = 193*1215;
 	double luas1box = lantaiP*8/1000000;
+	double jumlahboxparket,biayaparket,biayapemasangan,totalbiaya;
+	jumlahboxparket = luasrumah/luas1box;
+	biayaparket = jumlahboxparket * 500;
+	biayapemasangan = luasrumah * 20;
+	totalbiaya = biayaparket+biayapemasangan;
 
 }
