1c956cabad16eeeace68f85e4add08336971c2a5
AdrianMochtar <mochtaradrian.am@gmail.com>
2017-10-21 17:35:30 +0800

    menambah output hasil perhitungan

--- Praktek0204.cpp
+++ Praktek0204.cpp
@@ -10,5 +10,14 @@ int main(){
 	biayaparket = jumlahboxparket * 500;
 	biayapemasangan = luasrumah * 20;
 	totalbiaya = biayaparket+biayapemasangan;
+	cout << "Luas Rumah 		: "<<luasrumah<<" m2"<<endl;
+	cout << "Luas 1 box parket	: "<<luas1box<<" m2"<<endl;
+	cout << "Jumlah box parket 	: "<<jumlahboxparket<<endl;
+	cout << "Biaya Parket(Rp)	: "<< biayaparket<<endl;
+	cout << "Biaya Pasang(Rp) 	: "<< biayapemasangan<<endl;
+	cout << "Total Biaya(Rp)		: "<< totalbiaya<<endl;
+	
+	
+	
 
 }
